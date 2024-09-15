e401bbdc157139d9b8235fe449a08720ab0c97db
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-15 10:05:38 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	let trimStr = str.trim();
-	let words = trimStr.split(" ");
-	return words[0];
+	const charCount = {};
+	for(let char of str){
+		charCount[char] = (charCount[char] || 0) + 1;
+	}
+	for(let char of str){
+		if(charCount[char] === 1)
+			return char;
+	}
 }
+return null;
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
